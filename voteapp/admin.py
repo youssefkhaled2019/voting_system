5b3f76b780beda740a96ad7cc49a32b5d7c17670
youssefkhaled2019 <43790986+youssefkhaled2019@users.noreply.github.com>
2024-09-05 23:48:16 +0200

    v1

---
+++ voteapp/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Category, CategoryItem
+# Register your models here.
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    # pass
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(CategoryItem)
